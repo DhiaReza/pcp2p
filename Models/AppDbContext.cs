17435712d6a4b27f4291f0e3b6d0b868bd33f343
Fahreza Dhia <fahrezadhia16@gmail.com>
2026-01-16 16:49:22 +0700

    initial project init

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace pcp2p.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options)
+        {
+        }
+        public DbSet
+    }
+}
